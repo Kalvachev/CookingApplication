c688a6065f795104586ded44ac0a4309ce406596
Alexander <akalvachev99@gmail.com>
2021-03-21 19:04:51 +0200

    Create simple structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RecipeList from './RecipeList'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RecipeList recipes={sampleRecipes} />
+  )
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Chicken',
+    servings: 7,
+    cookTime: '1:30',
+    instructions: "1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken"
+  },
+  {
+    id: 2,
+    name: 'Pork',
+    servings: 6,
+    cookTime: '0:55',
+    instructions: "1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork"
+  }
+]
 
 export default App;
